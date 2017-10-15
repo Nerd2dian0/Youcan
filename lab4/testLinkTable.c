d201b11c55fc35c17d86c8ea972597e8cbf875fe
Nerd2dian0 <32013937+Nerd2dian0@users.noreply.github.com>
2017-10-15 19:06:55 +0800

    Update testLinkTable.c

--- lab4/testLinkTable.c
+++ lab4/testLinkTable.c
@@ -1,4 +1,4 @@
-/*接口的使用声明*/
+/*接口的使用说明*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
@@ -19,5 +19,43 @@ int main()
 {
   int i;
   tLinkTable * pLinkTable = CreateLinkTable();
-  assert(pLinkTable != NULL)
+  if (pLinkTable == NULL)
+  {
+    printf("CreateLinkTable Error!\n");
+    exit(0);
+  }
+  
+  /*在pNext节点后再创建新的节点，但并没有说明链表的具体内容*/
+  for(i = 0; i<10; i++)
+  {
+      tNode* pNode =(tNode*)malloc(sizeof(tNode));/*生成新的节点pNode*/
+      pNode->data = i;
+      debug("AddLinkTableNode\n");
+      AddLinkTableNode(pLinkTable,(tLinkTableNode *)pNode);/*将节点pNode强制转化，然后插入*/
+  }
+}
+
+/*不知道链表内的数据，怎么实现查询？-- search one by one的方式查询*/
+tNode * pTempNode = Search(pLinkTable);
+printf("%d\n",pTempNode->data);
+debug("DelLinkTableNode\n");
+DelLinkTableNode(pLinkTable,(tLinkTableNode *)pTempNode);
+free(pTempNode);
+DeleteLinkTable(pLinkTable);
+
+/*function of Search（）*/
+tNode * Search(tLinkTable *pLinkTable)
+{
+  debug("Search GetLinkTableHead\n");
+  tNode * pNode = (tNode*)GetLinkTableHead(pLinkTable);
+  while(pNode !=NULL)
+  {
+    if(pNode->data == 5)
+    {
+      return pNode;
+    }
+    debug("GetNextLinkTableNode\n");
+    pNode = (tNode*)GetNextLinkTableNode(pLinkTable,)
+  }
+  return NULL;
 }
