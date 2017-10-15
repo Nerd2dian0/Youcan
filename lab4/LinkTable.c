4b79045628892dc77cff47a187de6dc4048a9dd0
Nerd2dian0 <32013937+Nerd2dian0@users.noreply.github.com>
2017-10-15 22:44:31 +0800

    Create LinkTable.c

---
+++ lab4/LinkTable.c
@@ -0,0 +1,9 @@
+/*LinkTable.c是接口的源代码*/
+#include<stdio.h>
+#include<stdlib.h>
+#include "LinkTable.h"
+
+tLinkTable * CreateLinkTable()
+{
+  tLinkTable *pLinkTable = (tLinkTable*)malloc(sizeof(tLinkTable))；
+}
