3a8064bacdcc907dd3ff346ea7916c425e025d6e
Nerd2dian0 <32013937+Nerd2dian0@users.noreply.github.com>
2017-11-05 16:17:18 +0800

    Update test.c

--- lab6/test.c
+++ lab6/test.c
@@ -3,6 +3,26 @@
 #include<stdio.h>
 #include<unistd.h>
 
+int Info(int argc, char** argv)
+{
+    opterr = 0;
+    int oc;
+    while((oc = getopt(argc, argv, "nv")) != -1)
+      {
+        switch(oc)
+        {
+            case 'n':
+                printf("The author name is Nerd2dian0.\n");
+                break;
+            case 'v':
+                printf("The version is 1.01.\n");
+                break;
+        }
+    }
+   return 0;
+}
+}
+
 int main(int argc, char** argv)
 {
     /*char* cmd;
@@ -10,7 +30,7 @@ int main(int argc, char** argv)
     int (*handler)();*/
     MenuConfig("help", "This is help cmd", Help);
     MenuConfig("quit", "Quit the program", Quit);
-    MenuConfig("info", "Show information", NULL);
+    MenuConfig("info", "Show information", Info);
     MenuConfig("echo", "Repeat your input", NULL);
     MenuConfig("time", "Show time now", NULL);
     ExecuteMenu();
